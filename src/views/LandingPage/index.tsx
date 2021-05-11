53ca869e79440d4d70bb17b29fc4463a628fd675
cc0407 <cc0407@live.ca>
2021-05-11 00:36:54 -0400

    added state for board, hover animation

--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box } from '../../components/Box';
 
-const board = [null, null, null, null, null, null, null, null, null];
-
 export const Landing = () => {
+    const [board, setBoard] = useState([null, null, null, null, null, null, null, null, null]);
     return (
         <div className="window">
             <div className="viewport">
                 <div className="title">Welcome!</div>
                 <div className="gameBoard">
                     {board.map((id, index) => (
-                        <Box id={index} />
+                        <Box 
+                        id={index} 
+                        />
                     ))}
                 </div>
             </div>
