50e32fc060eb7d1cf2b24c757bc14aeff38f652f
cc0407 <cc0407@live.ca>
2021-05-11 00:31:04 -0400

    added grid of boxes using an array
    array will be moved to state

--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Box } from '../../components/Box';
 
+const board = [null, null, null, null, null, null, null, null, null];
 
 export const Landing = () => {
     return (
         <div className="window">
             <div className="viewport">
                 <div className="title">Welcome!</div>
-                <Box id={0} />
+                <div className="gameBoard">
+                    {board.map((id, index) => (
+                        <Box id={index} />
+                    ))}
+                </div>
             </div>
         </div>
     )
