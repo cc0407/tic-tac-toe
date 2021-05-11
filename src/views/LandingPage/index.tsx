538d227bb86774d9e4246a89ff0f01c3393fbf7d
cc0407 <cc0407@live.ca>
2021-05-11 19:21:35 -0400

    newGame button shell, css changes
    - Updated css, changed background colour and added boxShadow
    - added state object for if game won or not
    - Added barebones of button to start a new game after previous is won

--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -4,6 +4,7 @@ import { Box } from '../../components/Box';
 export const Landing = () => {
     const [board, setBoard] = useState([null, null, null, null, null, null, null, null, null]);
     const [letter, setLetter] = useState('X');
+    const [gameWon, setGameWon] = useState(false);
     return (
         <div className="window">
             <div className="viewport">
@@ -16,9 +17,15 @@ export const Landing = () => {
                         id={id} 
                         onClick={()=>{
                             if(id == null) {
+                                //update board
                                 const boardCopy = [...board];
                                 boardCopy[index] = letter;
                                 setBoard(boardCopy);
+
+                                //check if theres a winner
+
+
+                                //swap letter
                                 if (letter == 'X') {
                                     setLetter('O');
                                 }
@@ -29,6 +36,9 @@ export const Landing = () => {
                         }}
                         />
                     ))}
+                </div>
+                <div className="confirmButton">
+                    New Game
                 </div>
             </div>
         </div>
