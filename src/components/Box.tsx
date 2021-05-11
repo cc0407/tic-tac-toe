584600b02407bb2d514a9877cb1fe1ccd2481c0d
cc0407 <cc0407@live.ca>
2021-05-11 00:20:58 -0400

    created component for each box

---
+++ src/components/Box.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export const Box = ({id}) => {
+    return(
+        <div className="box">
+            {id}
+        </div>
+    );
+};
--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Box } from '../../components/Box';
 
 
 export const Landing = () => {
@@ -6,6 +7,7 @@ export const Landing = () => {
         <div className="window">
             <div className="viewport">
                 <div className="title">Welcome!</div>
+                <Box id={0} />
             </div>
         </div>
     )
