216236a562fe56c58519c6c64c31ee2acf3d4f3b
cc0407 <cc0407@live.ca>
2021-05-12 00:08:22 -0400

    added conditional text, changes gamewon state
    - added text for win and draw
    - changed gameWon state from boolean to int to allow for draw condition

--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -4,19 +4,21 @@ import { Box } from '../../components/Box';
 export const Landing = () => {
     const [board, setBoard] = useState([null, null, null, null, null, null, null, null, null]);
     const [letter, setLetter] = useState('X');
-    const [gameWon, setGameWon] = useState(false);
+    const [gameWon, setGameWon] = useState(0);
     return (
         <div className="window">
             <div className="viewport">
                 <div className="title">Welcome!</div>
-                <div>{gameWon ? `Winner:`:`Active player:`} {letter}</div>
+                
+                <div>{findAppropriateSentence(gameWon, letter)}</div>
+
                 <div className="gameBoard">
                     {board.map((id, index) => (
                         <Box 
                         key={index}
                         id={id} 
                         onClick={()=>{
-                            if(id == null && gameWon == false) {
+                            if(id == null && gameWon == 0) {
                                 //update board
                                 const boardCopy = [...board];
                                 boardCopy[index] = letter;
@@ -24,7 +26,7 @@ export const Landing = () => {
 
                                 //check if theres a winner
                                 var won = checkForWinner(boardCopy,letter);
-                                if (won) {
+                                if (won != 0) {
                                     setGameWon(won);
                                     return;
                                 }
@@ -46,7 +48,7 @@ export const Landing = () => {
                     onClick={() => {
                         setBoard([null, null, null, null, null, null, null, null, null]);
                         setLetter('X');
-                        setGameWon(false);
+                        setGameWon(0);
                     }}
                 >
                     New Game
@@ -57,26 +59,40 @@ export const Landing = () => {
 }
 
 function checkForWinner(board, letter) {
-    var won = false;
+    var won = 0;
     //col
     for (var i = 0; i < 3; i++) {
         if(board[i] === letter && board[i+3] === letter && board[i+6] === letter) {
-            won = true;
+            won = 1;
         }
     }
     //row
     for (var i = 0; i <= 6; i += 3) {
         if(board[i] === letter && board[i+1] === letter && board[i+2] === letter) {
-            won = true;
+            won = 1;
         }
     }
     //diag
     if(board[0] === letter && board[4] === letter && board[8] === letter) {
-        won = true;
+        won = 1;
     }
     else if(board[2] === letter && board[4] === letter && board[6] === letter) {
-        won = true;
+        won = 1;
     }
 
+    if(!board.includes(null) && won == 0) {
+        won = 2;
+    }
     return won;
+}
+
+function findAppropriateSentence (gameWon, letter) {
+    switch (gameWon){
+        case 0:
+            return `Active Player: ` + letter;
+        case 1:
+            return `Winner: ` + letter;
+        default:
+            return `Draw!`
+    }
 }
