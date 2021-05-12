345f4113d8603c616a019d034b99c38a4afa94f3
cc0407 <cc0407@live.ca>
2021-05-12 00:07:22 -0400

    calculated win conditions

--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -9,29 +9,34 @@ export const Landing = () => {
         <div className="window">
             <div className="viewport">
                 <div className="title">Welcome!</div>
-                <div>Active player: {letter}</div>
+                <div>{gameWon ? `Winner:`:`Active player:`} {letter}</div>
                 <div className="gameBoard">
                     {board.map((id, index) => (
                         <Box 
                         key={index}
                         id={id} 
                         onClick={()=>{
-                            if(id == null) {
+                            if(id == null && gameWon == false) {
                                 //update board
                                 const boardCopy = [...board];
                                 boardCopy[index] = letter;
                                 setBoard(boardCopy);
 
                                 //check if theres a winner
-
-
-                                //swap letter
-                                if (letter == 'X') {
-                                    setLetter('O');
+                                var won = checkForWinner(boardCopy,letter);
+                                if (won) {
+                                    setGameWon(won);
+                                    return;
                                 }
                                 else {
-                                    setLetter('X');
-                                }
+                                    //swap letter
+                                    if (letter === 'X') {
+                                        setLetter('O');
+                                    }
+                                    else {
+                                        setLetter('X');
+                                    }
+                                } 
                             }
                         }}
                         />
@@ -49,4 +54,29 @@ export const Landing = () => {
             </div>
         </div>
     )
+}
+
+function checkForWinner(board, letter) {
+    var won = false;
+    //col
+    for (var i = 0; i < 3; i++) {
+        if(board[i] === letter && board[i+3] === letter && board[i+6] === letter) {
+            won = true;
+        }
+    }
+    //row
+    for (var i = 0; i <= 6; i += 3) {
+        if(board[i] === letter && board[i+1] === letter && board[i+2] === letter) {
+            won = true;
+        }
+    }
+    //diag
+    if(board[0] === letter && board[4] === letter && board[8] === letter) {
+        won = true;
+    }
+    else if(board[2] === letter && board[4] === letter && board[6] === letter) {
+        won = true;
+    }
+
+    return won;
 }
